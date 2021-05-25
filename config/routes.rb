64fc854a270851cff22e974a165f0e815a0bdcc7
Theresa Wendelstadt <theresa@wendelstadt.de>
2021-05-25 11:30:47 +0200

    controllers and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tables, except: [:update, :edit] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:update, :edit] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  get '/dashboard', to: 'dashboards#show'
+  get '/users/:id', to: 'users#show'
+
 end
