609c7f6ada155af5add0da3c7c0675844bf71f36
Max Rapp <rappmax@gmail.com>
2021-05-25 14:15:49 +0200

    added controller and broken view

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,2 +1,20 @@
 class TablesController < ApplicationController
+  def new
+    @table = Table.new
+  end
+
+  def create
+    @table = table.new(table_params)
+    if @table.save
+      redirect_to table_path(@table)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def table_params
+    params.require(:table).permit(:address, :rating, :description, :picture)
+  end
 end
