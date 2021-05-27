c217fbb0bc8fa5ccb49ea2a6fff6919fb404af98
Max Rapp <rappmax@gmail.com>
2021-05-27 17:38:17 +0200

    search v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,16 +18,16 @@ User.destroy_all
 vincent = User.create(email:"vincent@gmail.com", password: "123456")
 
 file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
-table = Table.create(name: 'New Game', description: "Table is outdoors", user_id: vincent.id)
+table = Table.create(name: 'New Game', description: "Table is outdoors", user_id: vincent.id, date: Date.new(2021,5,28))
 table.picture.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 
 
 file_two = URI.open('https://images.unsplash.com/photo-1611251135345-18c56206b863?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-table_two = Table.create(address: "T-Centralen", description: "Great table tennis surface", user_id: vincent.id)
+table_two = Table.create(address: "T-Centralen", description: "Great table tennis surface", user_id: vincent.id, date: Date.new(2021,5,27))
 table_two.picture.attach(io: file_two, filename: 'nes.png', content_type: 'image/png')
 
 file_two = URI.open('https://images.unsplash.com/photo-1611251135345-18c56206b863?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-table_two = Table.create(address: "Atlasgatan 4", description: "Great net and bats", user_id: vincent.id)
+table_two = Table.create(address: "Atlasgatan 4", description: "Great net and bats", user_id: vincent.id, date: Date.new(2021,5,27))
 table_two.picture.attach(io: file_two, filename: 'nes.png', content_type: 'image/png')
 
 file_three = URI.open('https://images.unsplash.com/photo-1611251135345-18c56206b863?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
